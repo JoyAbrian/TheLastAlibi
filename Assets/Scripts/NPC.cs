23a04933ba13a2c2d7ec1bf08898d65d8816cc14
anisanhy <joyabrianrantepasang14@gmail.com>
2025-06-16 20:29:08 +0800

    create NPC

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    public string name;
+    public string personality;
+
+    [Header("Reaction Sprites")]
+    public Sprite angrySprite;
+    public Sprite concernedSprite;
+    public Sprite happySprite;
+    public Sprite neutralSprite;
+    public Sprite smileSprite;
+}
